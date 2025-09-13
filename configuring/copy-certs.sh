7874f628a3f57d557049dad37adb6b8d9d09f299
PowerPixel <52136144+PowerPixel@users.noreply.github.com>
2025-09-13 20:49:06 +0200

    force overwrite of certificates for configuration step

--- configuring/copy-certs.sh
+++ configuring/copy-certs.sh
@@ -7,26 +7,26 @@ authdir="$dir/../auth"
 source "$authdir/auth-vars.sh"
 
 for cert in ${commoncerts[@]}; do
-    cp $authdir/$cert $dir/roles/install-control-certs/files
-    cp $authdir/$cert $dir/roles/install-worker-certs/files
+    cp -rf $authdir/$cert $dir/roles/install-control-certs/files
+    cp -rf $authdir/$cert $dir/roles/install-worker-certs/files
 done
 
 for cert in ${workercerts[@]}; do
-    cp $authdir/$cert $dir/roles/install-worker-certs/files
+    cp -rf $authdir/$cert $dir/roles/install-worker-certs/files
 done
 
 for node in ${workers[@]}; do 
-    cp $authdir/$node.pem $dir/roles/install-worker-certs/files
-    cp $authdir/$node-key.pem $dir/roles/install-worker-certs/files
-    cp $authdir/$node.kubeconfig $dir/roles/install-worker-certs/files
+    cp -rf $authdir/$node.pem $dir/roles/install-worker-certs/files
+    cp -rf $authdir/$node-key.pem $dir/roles/install-worker-certs/files
+    cp -rf $authdir/$node.kubeconfig $dir/roles/install-worker-certs/files
 done
 
 for cert in ${controlcerts[@]}; do
-    cp $authdir/$cert $dir/roles/install-control-certs/files
+    cp -rf $authdir/$cert $dir/roles/install-control-certs/files
 done
 
 for node in ${control[@]}; do 
-    cp $authdir/$node.pem $dir/roles/install-control-certs/files
-    cp $authdir/$node-key.pem $dir/roles/install-control-certs/files
-    cp $authdir/$node.kubeconfig $dir/roles/install-control-certs/files
+    cp -rf $authdir/$node.pem $dir/roles/install-control-certs/files
+    cp -rf $authdir/$node-key.pem $dir/roles/install-control-certs/files
+    cp -rf $authdir/$node.kubeconfig $dir/roles/install-control-certs/files
 done
