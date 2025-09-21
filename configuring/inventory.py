f21c3c5f3e9530c283f34bef51adc22dfffb0996
PowerPixel <52136144+PowerPixel@users.noreply.github.com>
2025-09-21 17:07:26 +0200

    add ids to host in dynamic python inventory

--- configuring/inventory.py
+++ configuring/inventory.py
@@ -10,25 +10,37 @@ GATEWAY_GROUP="gateway"
 SSH_TUNNEL=f"-J ubuntu@{GATEWAY_EXPOSED_IP}"
 SSH_TUNNEL_KEY="ansible_ssh_common_args"
 
+hostvars=dict()
+
 def generate_gateway_hosts() -> dict:
     gateway = dict()
     gateway["hosts"] = [ GATEWAY_EXPOSED_IP ]
     return gateway
 
 def generate_workers_hosts() -> dict:
+    global hostvars
     workers = dict()
     hosts = []
     for i in range(0, NB_WORKERS):
-        hosts.append(f"worker{i}")
+        hostname=f"worker{i}"
+        vars=dict()
+        hosts.append(hostname)
+        vars["id"] = i
+        hostvars[hostname]=vars
     workers["hosts"] = hosts
     workers["vars"] = { SSH_TUNNEL_KEY: SSH_TUNNEL }
     return workers
 
 def generate_control_hosts() -> dict:
+    global hostvars
     control = dict()
     hosts = []
-    for i in range(0, NB_WORKERS):
-        hosts.append(f"control{i}")
+    for i in range(0, NB_CONTROL):
+        hostname=f"control{i}"
+        vars=dict()
+        hosts.append(hostname)
+        vars["id"] = i
+        hostvars[hostname]=vars
     control["hosts"] = hosts
     control["vars"] = { SSH_TUNNEL_KEY: SSH_TUNNEL }
     return control
@@ -37,5 +49,6 @@ inventory = dict()
 inventory[GATEWAY_GROUP] = generate_gateway_hosts()
 inventory[WORKERS_GROUP] = generate_workers_hosts()
 inventory[CONTROL_GROUP] = generate_control_hosts()
+inventory["hostvars"] = hostvars
 
 print(json.dumps(inventory))
